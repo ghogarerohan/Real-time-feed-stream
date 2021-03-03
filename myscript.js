37432324621bf4ee795adc0b3f14e0ebcf248ebe
ghogarerohan <ghogare.rohan@gmail.com>
2021-03-03 22:45:58 +0530

    First Commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+load();
+
+function load() {
+var feed ="http://feeds.bbci.co.uk/news/world/rss.xml"; 
+new GFdynamicFeedControl(feed, "feedControl"); 
+}
+google.load("feeds", "1"); 
+google.setOnLoadCallback(load); 
